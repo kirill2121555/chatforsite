4fdec6e6d27d181e56c349d283c279a3d05fcc3b
kirill2121555 <kirill2121555@gmail.com>
2022-08-24 21:28:34 +0300

    first commit

--- index.js
+++ index.js
@@ -9,17 +9,33 @@ const cors = require('cors')
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: '*',
+    origin:'*',
     methods: ["GET", "POST"]
   }
 })
 
-app.use(cors(corsOptions));
 
-app.get('/oo', function (req, res) {
+app.get('/oo', function(req, res) {
   res.json('helloworld');
 });
 
+io.on('connection', (socket) => {
+  try{
+  socket.on('message', async (data) => {
+    
+    const user = await userModel.findById(data.i)
+    const isdialog = await user?.dialogs.get(data.to)
+    let dialog=[];
+    dialog = await dialogModel.findById(isdialog)
+    dialog.messages.push({ username: data.username, message: data.message })
+    dialog.save()
+    socket.join(dialog.room_uuid)
+    io.to(dialog.room_uuid).emit('message', JSON.stringify(data));
+  
+
+  })}catch(e){
+    console.log('error')  }
+});
 
 const start = async () => {
   try {
