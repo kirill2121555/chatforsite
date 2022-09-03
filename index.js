daac2998adefed3d50d5fbd9adfea5d52cd0c594
kirill2121555 <kirill2121555@gmail.com>
2022-09-03 23:16:01 +0300

    adding a photo to a chat

--- index.js
+++ index.js
@@ -6,38 +6,50 @@ const app = express()
 const server = require("http").Server(app);
 const PORT = process.env.PORT || 9999
 const cors = require('cors')
+const fs = require("fs");
+
 
 const io = require("socket.io")(server, {
   cors: {
-    origin:'https://fronthelp.vercel.app',
+    origin: 'https://fronthelp.vercel.app',//   http://localhost:3000
     methods: ["GET", "POST"],
     path: "/my-custom-path/",
-    credentials: true  }
+    credentials: true
+  }
 })
 
 
-app.get('/oo', function(req, res) {
+app.get('/oo', function (req, res) {
   console.log('oooo')
   res.json('helloworld');
 });
-console.log('mess')
 
 io.on('connection', (socket) => {
-  try{
-  socket.on('message', async (data) => {
-    console.log('mess')
-    const user = await userModel.findById(data.i)
-    const isdialog = await user?.dialogs.get(data.to)
-    let dialog=[];
-    dialog = await dialogModel.findById(isdialog)
-    dialog.messages.push({ username: data.username, message: data.message })
-    dialog.save()
-    socket.join(dialog.room_uuid)
-    io.to(dialog.room_uuid).emit('message', JSON.stringify(data));
-  
+  try {
+    socket.on('message', async (data) => {
+      const user = await userModel.findById(data.i)
+      const isdialog = await user.dialogs.get(data.to)
+      let dialog = [];
+      dialog = await dialogModel.findById(isdialog)
+      dialog.messages.push({ username: data.username, message: data.message })
+      dialog.save()
+      socket.join(dialog.room_uuid)
+      io.to(dialog.room_uuid).emit('message', JSON.stringify(data));
+    })
 
-  })}catch(e){
-    console.log('error')  }
+    socket.on('img', async (imgData) => {
+      const user = await userModel.findById(imgData.i)
+      const isdialog = await user?.dialogs.get(imgData.to)
+      let dialog = [];
+      dialog = await dialogModel.findById(isdialog)
+      dialog.messages.push({ username: imgData.username, message: imgData.message })
+      dialog.save()
+      socket.join(dialog.room_uuid)
+      io.to(dialog.room_uuid).emit('newImg', JSON.stringify(imgData));
+    });
+  } catch (e) {
+    console.log('error')
+  }
 });
 
 const start = async () => {
